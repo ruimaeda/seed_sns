c9cc7d66f5469d653d152283ed74577d77b80df9
ruimaeda <louis@tabippo.net>
2017-05-25 19:07:57 +0800

    jsの練習問題をプッシュ

---
+++ js_sample/color_fruits.js
@@ -0,0 +1,23 @@
+function color_alert_red() {
+	alert("赤です");
+}
+
+function color_alert_blue() {
+	alert("青です");
+}
+
+function color_alert_yellow() {
+	alert("黄です");
+}
+
+function fruits_alert_apple() {
+	document.getElementById('fruit').textContent = 'りんご!';
+}
+
+function fruits_alert_orange() {
+	document.getElementById('fruit').textContent = 'オレンジ!';
+}
+
+function fruits_alert_banana() {
+	document.getElementById('fruit').textContent = 'バナナ!';
+}
